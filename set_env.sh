7f2e7d96add5138ace4a1988f7dcd5b06bb13614
Nanda <man00201@echristus.net>
2020-07-26 16:41:06 -0500

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=maninder
+export POSTGRES_PASSWORD=ashmeet167
+export POSTGRES_HOST=maninderdatabase.chvxs6j1swtz.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=maninderdatabase
+export AWS_BUCKET=maninderproject3finalver
+export AWS_REGION=us-east-2
+export AWS_PROFILE=
+export JWT_SECRET=
+export URL=http://localhost:8100
